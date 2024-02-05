c909f639be9b9173dc20205eb773a9f07660fcf5
Joomun <joomunmuddathir@gmail.com>
2024-02-05 12:30:57 +0400

    Init

---
+++ js/index_script.js
@@ -0,0 +1,4 @@
+document.getElementById('start-voice').addEventListener('click', function() {
+    const output = document.getElementById('voice-output');
+    output.textContent = 'Voice assistance activated... (mock functionality)';
+});
